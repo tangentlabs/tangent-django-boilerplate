2f85f5cc1a84784fa56618bb873b2bd3d6e9bb01
David Winterbottom <david.winterbottom@gmail.com>
2012-10-02 16:16:24 +0100

    Fix silly bug in DISABLED_APPs handling
    This has been broken for ages.

--- www/settings.py
+++ www/settings.py
@@ -25,18 +25,14 @@ if 'EXTRA_APPS' in locals():
 if 'DISABLED_APPS' in locals():
     INSTALLED_APPS = [k for k in INSTALLED_APPS if k not in DISABLED_APPS]
     MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
-    for a in DISABLED_APPS:
-        for x, m in enumerate(MIDDLEWARE_CLASSES):
-            if m.startswith(a):
-                MIDDLEWARE_CLASSES.pop(x)
     TEMPLATE_CONTEXT_PROCESSORS = list(TEMPLATE_CONTEXT_PROCESSORS)
-    for x, m in enumerate(TEMPLATE_CONTEXT_PROCESSORS):
-        if m.startswith(a):
-            TEMPLATE_CONTEXT_PROCESSORS.pop(x)
-    DATABASE_ROUTERS = list(DATABASE_ROUTERS)
-    for x, m in enumerate(DATABASE_ROUTERS):
-        if m.startswith(a):
-            DATABASE_ROUTERS.pop(x)
+    for app_name in DISABLED_APPS:
+        for x, m in enumerate(MIDDLEWARE_CLASSES):
+            if m.startswith(app_name):
+                MIDDLEWARE_CLASSES.pop(x)
+        for x, m in enumerate(TEMPLATE_CONTEXT_PROCESSORS):
+            if m.startswith(app_name):
+                TEMPLATE_CONTEXT_PROCESSORS.pop(x)
 
 # Keep version number here - this is generally overwritten as
 # part of deployment to be the build name
