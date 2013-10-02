b9fd92daf1032d8bd675486ef5e1fb2aeb8d14c3
David Winterbottom <david.winterbottom@gmail.com>
2013-10-02 12:00:26 +0100

    Fix issue with imported settings not being available
    They need to be copied into the local namespace.

--- www/settings.py
+++ www/settings.py
@@ -1,3 +1,9 @@
 import os
 
+# Use an env variable to determine which settings file to import.  Then copy
+# all variables into the local namespace.
 __import__(os.environ.get('DJANGO_CONF', 'conf.local'), globals(), locals(), ['*'])
+module = __import__(os.environ.get('DJANGO_CONF', 'conf.local'), globals(), locals(), ['*'])
+for k in dir(module):
+    if not k.startswith("__"):
+        locals()[k] = getattr(module, k)
