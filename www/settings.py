4e0f4accacf73719d41705608a4b1e46280712bf
David Winterbottom <david.winterbottom@gmail.com>
2013-10-07 15:44:02 +0100

    Allow conf/local.py to be in source control

--- www/settings.py
+++ www/settings.py
@@ -2,8 +2,15 @@ import os
 
 # Use an env variable to determine which settings file to import.  Then copy
 # all variables into the local namespace.
-__import__(os.environ.get('DJANGO_CONF', 'conf.local'), globals(), locals(), ['*'])
-module = __import__(os.environ.get('DJANGO_CONF', 'conf.local'), globals(), locals(), ['*'])
-for k in dir(module):
-    if not k.startswith("__"):
-        locals()[k] = getattr(module, k)
+
+# If you want custom settings, create a new settings file (eg conf.barry) and
+# import * from conf.local then apply your overrides.
+conf_module = os.environ.get('DJANGO_CONF', 'conf.local')
+try:
+    module = __import__(conf_module, globals(), locals(), ['*'])
+except ImportError:
+    print "Unable to import %s" % conf_module
+else:
+    for k in dir(module):
+        if not k.startswith("__"):
+            locals()[k] = getattr(module, k)
