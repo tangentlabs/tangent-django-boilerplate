bfa269186fe95b9f0b7ff59ba2c96c50e1f0e886
David Winterbottom <david.winterbottom@gmail.com>
2013-10-08 14:02:33 +0100

    Allow test runner to take args

--- www/settings_test.py
+++ www/settings_test.py
@@ -2,12 +2,8 @@ from settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', 
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:',
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
