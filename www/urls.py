2b0c3e2de7aa8f9ed62b345a867569c500ebbecc
Francis Reyes <francis.mark.reyes@gmail.com>
2013-10-18 11:05:49 +1100

    Add missing imports

--- www/urls.py
+++ www/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.conf.urls import patterns, include
+from django.conf.urls import patterns, include, url
 
 admin.autodiscover()
 
@@ -18,6 +18,7 @@ if settings.DEBUG:
 
     # Allow FEDs to get arbitrary templates rendered and see a styleguide
     from django.shortcuts import render
+    from django.views import generic
     urlpatterns += patterns('',
         url(r'^templates/(?P<template_name>.*)$', render),
         url(r'^styleguide/$', generic.TemplateView.as_view(template_name='styleguide.html'))
