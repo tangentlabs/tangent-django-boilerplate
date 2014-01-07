46e137fafd698e671a721b5ed75b2c603818355b
Alex Moon <alex.moon@tangentlabs.co.uk>
2014-01-07 14:18:19 +0000

    "localhost" implies localhost over IP, which it is not, generally speaking

--- www/conf/local.py
+++ www/conf/local.py
@@ -11,7 +11,7 @@ DATABASES = {
         'NAME': location('db.sqlite3'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for peer (local non-IP socket). Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
 }
