4d656bff8f9589844cc4d471535bca72e4a6cfc0
David Winterbottom <david.winterbottom@gmail.com>
2014-05-20 10:39:27 +0100

    Tidy URLs module

--- www/urls_oscar.py
+++ www/urls_oscar.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
-from django.views import generic
 
 from oscar.app import application
 from oscar.views import handler500, handler404, handler403
@@ -30,9 +29,11 @@ if settings.DEBUG:
 
     # Allow FEDs to get arbitrary templates rendered and see a styleguide
     from django.shortcuts import render
+    from django.views import generic
     urlpatterns += patterns('',
         url(r'^templates/(?P<template_name>.*)$', render),
-        url(r'^styleguide/$', generic.TemplateView.as_view(template_name='styleguide.html'))
+        url(r'^styleguide/$', generic.TemplateView.as_view(
+            template_name='styleguide.html'))
     )
 
     # Do explicit setup of django debug toolbar
