513eb91e0f623298c6c346397c27ca062728c4ea
David Winterbottom <david.winterbottom@gmail.com>
2014-06-02 13:37:49 +0100

    Add example functional test using splinter

--- www/tests/functional_tests.py
+++ www/tests/functional_tests.py
@@ -11,3 +11,8 @@ def test_with_webtest(webtest):
 def test_with_firefox(live_server, firefox):
     firefox.get(live_server.url + '/')
     assert 'Hello' in firefox.page_source
+
+
+def test_with_splinter(live_server, browser):
+    browser.visit(live_server.url + '/')
+    assert browser.is_text_present("Hello")
