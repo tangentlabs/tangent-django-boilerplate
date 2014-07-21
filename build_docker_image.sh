f1cd25b190c6cafd8b59ca224b31c3362cda5d9b
David Winterbottom <david.winterbottom@gmail.com>
2014-07-21 22:27:58 +0100

    Reorganise Dockerfiles into one folder

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
 # 
-# Build the docker image ready for deployment
-
-# Immediately exit if a command exits with a non-zero status
-set -e
-
-# Ensure the Dockerfile symlink is removed if an EXIT signal is raised
-trap "[[ -h Dockerfile ]] && unlink Dockerfile" EXIT
+# Build a docker image ready for deployment
+#
+# This script symlinks in the appropriate Dockerfile then builds the image.
 
 # Set project name
 PROJECT_NAME=boilerplate
 
+set -e  # Fail fast
+
+# Ensure the Dockerfile symlink is removed if an EXIT signal is raised
+trap "[[ -h Dockerfile ]] && unlink Dockerfile" EXIT
+
+
 # Ensure script is called correctly
 usage() {
+    echo $1
     echo "Usage: $0 [base|dev|release] [TAG|VERSION]"
     exit 1
 }
-[[ -z $PROJECT_NAME ]] && usage "Missing project name"
 [[ -z $1 ]] && usage "Missing image type"
-[[ -z $2 ]] && usage "Missing tag"
 
 IMAGETYPE=$1
 TAG=${2:-latest}
 
 case $IMAGETYPE in
     base|dev|release)
-        DOCKERFILE="deploy/$IMAGETYPE/Dockerfile"
+        DOCKERFILE="deploy/docker/Dockerfile-$IMAGETYPE"
         if [ ! -f "$DOCKERFILE" ]; then
-            echo "Missing $ROLE Dockerfile ($DOCKERFILE)"
+            echo "Missing $IMAGETYPE Dockerfile ($DOCKERFILE)"
             exit 1
         else 
             # Symlink the appropriate Dockerfile into place and run 'docker build'
             ln -sf $DOCKERFILE Dockerfile
-            DOCKER_TAG="$PROJECT_NAME-$IMAGETYPE-$ROLE:$TAG"
+            DOCKER_TAG="$PROJECT_NAME-$IMAGETYPE:$TAG"
             printf "Building Docker image $DOCKER_TAG\n\n"
             docker build -t $DOCKER_TAG .
         fi
     ;;
     *)
-        usage
+        usage "Unknown image type"
     ;;
 esac
