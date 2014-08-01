0232e419727c624a34dc65f742592de695969d97
David Winterbottom <david.winterbottom@gmail.com>
2014-08-01 16:36:36 +0100

    Correct minor issues picked up in code-review

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -12,12 +12,13 @@ set -e  # Fail fast
 # Ensure the Dockerfile symlink is removed if an EXIT signal is raised
 trap "[[ -h Dockerfile ]] && unlink Dockerfile" EXIT
 
-# Ensure script is called correctly
 usage() {
     echo $1
     echo "Usage: $0 [base|dev|release] [TAG|VERSION]"
     exit 1
 }
+
+# Ensure script is called correctly
 [[ -z $1 ]] && usage "Missing image type"
 
 IMAGETYPE=$1
--- www/utils/s3.py
+++ www/utils/s3.py
@@ -1,7 +1,9 @@
 from storages.backends.s3boto import S3BotoStorage
 
+
 class StaticRootS3BotoStorage(S3BotoStorage):
     location = 'static'
+
 
 class MediaRootS3BotoStorage(S3BotoStorage):
     location = 'media'
