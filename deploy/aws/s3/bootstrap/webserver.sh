6cb11cf5ec3f338899be325c9c8cc23f9cfbb587
David Winterbottom <david.winterbottom@gmail.com>
2014-08-18 14:39:11 +0100

    Improve the "webserver" bootstrap script
    The script now fetches the key details from S3 such as:
    
    - the hostnames nginx should respond to
    - the docker image to deploy
    - the sensitive env variables to pass

--- deploy/aws/s3/bootstrap/webserver.sh
+++ deploy/aws/s3/bootstrap/webserver.sh
@@ -2,81 +2,77 @@
 #
 # Bootstrap script for a webserver
 #
-# This file is just an example - it is intended to be customised for your project.
-# TODO Customise this file for your project
-# 
-# When ready, this file should be uploaded to S3 so it can be downloaded and
-# executed by the Stage 1 bootstrap script. Its S3 location will need to be set
-# as a tag against the EC2 instance.
+# This script is intended to be called by the user data script from an EC2
+# instance (as it requires the BUCKET_URL env variable to be set). This script
+# fetches 3 things that it needs to run the appropriate docker container:
+#
+# s3://$bucket/release/docker_image  -  A text file whose contents are a Docker
+# registry URL.
+#
+# s3://$bucket/release/hostnames  -  A text file whose contents are the
+# hostnames that nginx should respond to.
+#
+# s3://$bucket/release/env  -  A text file (not in source control) which is
+# downloaded so it can be passed into the Docker container
 
-# ===================================================================
-
-# Set your project name here
-PROJECT=boilerplate
-
-# Role of this container (eg webserver, search, queue)
-ROLE=webserver
-
-# This is the Docker Image Tag you want to pull
-TAG=latest
-
-# What is the address to configure the nginx vhost on to listen
-# for?
-HOSTNAMES=
-
-# Docker Repository to pull the image from
-DOCKER_IMAGE_NAME=docker.tangentlabs.co.uk/${PROJECT}-release-${ROLE}:${TAG}
-
-# S3 URI for htaccess file should it be needed. Leave empty to ignore password
-# protection for the site.
-HTACCESS_FILE=
-
-# ===================================================================
 
 set -e  # Fail fast
 
 exec 1> >(tee -a /var/log/bootstrap.webserver.log)
 exec 2>&1
 
-# Create a /containers/ subdirectory to act as a bridge
-# between the host and the container
-HOST_SHARED_FOLDER="/containers/${PROJECT}-${ROLE}"
-echo "Creating $HOST_SHARED_FOLDER"
-mkdir -p "$HOST_SHARED_FOLDER"
+# Determine docker image name from a S3 file
+IMAGE_S3_PATH="$BUCKET_URL/release/docker_image"
+echo "Fetching $IMAGE_S3_PATH"
+aws s3 cp $IMAGE_S3_PATH /tmp/docker_image
+DOCKER_IMAGE=$(cat /tmp/docker_image)
+
+# Determine hostnames from a S3 file
+IMAGE_S3_PATH="$BUCKET_URL/release/hostnames"
+echo "Fetching $IMAGE_S3_PATH"
+aws s3 cp $IMAGE_S3_PATH /tmp/hostnames
+HOSTNAMES=$(cat /tmp/hostnames)
+
+# Fetch sensitive env variables from S3
+ENV_S3_PATH="$BUCKET_URL/release/env"
+echo "Fetching $IMAGE_S3_PATH"
+aws s3 cp $IMAGE_S3_PATH /host/env
 
 # Fetch and run the docker image, passing in the S3 location of the
 # production settings file.
-echo "Pulling docker image '$DOCKER_IMAGE_NAME'"
-docker pull ${DOCKER_IMAGE_NAME}
-DOCKER_CONTAINER_NAME="$PROJECT-$ROLE-$TAG"
+echo "Pulling docker image '$DOCKER_IMAGE'"
+docker pull $DOCKER_IMAGE
 
 echo "Running docker container"
-docker run -d -p 80 --name $DOCKER_CONTAINER_NAME \
-           -v $HOST_SHARED_FOLDER:/host \
-           -e DJANGO_ENV_URI=s3://${PROJECT}/env/prod \
-           ${DOCKER_IMAGE_NAME}
+docker run -d -p 80 --name webserver \
+           -v /host:/host \
+           -e DJANGO_ENV_URI=/host/env \
+           $DOCKER_IMAGE_NAME
 
 # Port 80 inside the container will be bound to a random
 # port on the host. Find out what it is!
 DOCKER_PORT=$(docker port $DOCKER_CONTAINER_NAME 80)
 
 # Ensure nginx is installed
+echo "Installing nginx to proxy to port $DOCKER_PORT of container"
 apt-get install -y nginx
 find /etc/nginx/sites-enabled/ -type f -delete
-[ -n "$HTACCESS_FILE" ] && aws s3 cp $HTACCESS_FILE /etc/nginx/htpasswd
 
 cat > /etc/nginx/sites-enabled/docker.conf << EOF
+# Default server
+server {
+    return 404;
+}
+
 upstream docker {
     server 127.0.0.1:$DOCKER_PORT;
 }
 
 server {
     listen 80;
-    server_name ${HOSTNAMES};
+    server_name $HOSTNAMES;
 
     location / {
-        $([[ -n $HTACCESS_FILE ]] && printf "auth_basic \"Restricted\";")
-        $([[ -n $HTACCESS_FILE ]] && printf "auth_basic_user_file /etc/nginx/htpasswd;")
         include uwsgi_params;
         uwsgi_pass docker;
     }
