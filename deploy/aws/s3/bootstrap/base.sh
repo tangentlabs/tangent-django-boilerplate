0890e8d8bd11d9df512310cb8a9df8b179e41cb4
David Winterbottom <david.winterbottom@gmail.com>
2014-08-18 14:27:18 +0100

    Download things quietly in base bootstrap script
    And create the /host/ folder.

--- deploy/aws/s3/bootstrap/base.sh
+++ deploy/aws/s3/bootstrap/base.sh
@@ -20,16 +20,19 @@ function notify {
     printf "\n\n==== $1 ====\n\n"
 }
 
+notify "Creating folder to mount into Docker container"
+mkdir -p /host/
+
 notify "Installing Docker"
-wget -O - https://get.docker.io/ | bash
+wget -q -O - https://get.docker.io/ | bash
 
 notify "Installing nsenter"
-wget -O /usr/local/bin/nsenter https://github.com/tangentlabs/nsenter/raw/master/nsenter
+wget -q -O /usr/local/bin/nsenter https://github.com/tangentlabs/nsenter/raw/master/nsenter
 chmod +x /usr/local/bin/nsenter
 
 # TODO Set the auth key here
 # DOCKER_REGISTRY_AUTH=
 notify "Creating Docker credentials Files"
-[[ ! -z "$DOCKER_REGISTRY_AUTH" ]] && echo "Docker registry auth missing!" && exit 1 
+[[ -z "$DOCKER_REGISTRY_AUTH" ]] && echo "Docker registry auth missing!" && exit 1 
 echo '{"https://docker.tangentlabs.co.uk/v1/":{"auth":"' $DOCKER_REGISTRY_AUTH '","email":""}}' > /root/.dockercfg
 echo '{"https://docker.tangentlabs.co.uk/v1/":{"auth":"' $DOCKER_REGISTRY_AUTH '","email":""}}' > /home/ubuntu/.dockercfg
