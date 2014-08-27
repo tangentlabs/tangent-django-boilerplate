93a10bfd113d6116a3df24050601dfb07e611df5
David Winterbottom <david.winterbottom@gmail.com>
2014-08-27 09:35:50 +0100

    Statics are automatically served in debug mode
    So no need for adding static serving to the URLs config.

--- www/urls.py
+++ www/urls.py
@@ -23,10 +23,8 @@ urlpatterns = patterns(
 )
 
 if settings.DEBUG:
-    # Render statics/media locally
+    # Render media locally
     from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
