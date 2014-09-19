713a47e0c184f9f139e01d655ed939eef6c1f7c2
David Winterbottom <david.winterbottom@gmail.com>
2014-09-19 21:30:39 +0100

    Create the logs folder before we try and write to it

--- deploy/docker/start_webserver.sh
+++ deploy/docker/start_webserver.sh
@@ -21,6 +21,9 @@ set -e  # Fail fast
 [ -z "$DJANGO_ENV_URI" ] && error "A DJANGO_ENV_URI env variable must be specified"
 [ ! -f "$DJANGO_ENV_URI" ] && error "$DJANGO_ENV_URI does not exist"
 
+# Ensure there is a folder to log to
+[[ ! -d /host/logs ]] && mkdir -p /host/logs
+
 # Copy all output to file
 exec 1> >(tee -a /host/logs/docker.container_startup.log)
 exec 2>&1
@@ -28,8 +31,6 @@ exec 2>&1
 # When the container starts is useful audit information
 printf "\n%s - Starting container with DJANGO_ENV_URI=%s\n\n" "$(date)" $DJANGO_ENV_URI
 
-# Ensure there is a folder to log to
-[[ ! -d /host/logs ]] && mkdir -p /host/logs
 
 echo "Linking /var/www/.env to $DJANGO_ENV_URI"
 ln -s $DJANGO_ENV_URI /var/www/.env
