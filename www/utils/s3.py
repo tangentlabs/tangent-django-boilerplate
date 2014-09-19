5688d938778fc5073d85d36b30755d63a2c22362
David Winterbottom <david.winterbottom@gmail.com>
2014-09-19 21:27:29 +0100

    Strip aws-security-token query param from static URLs
    This is due to a bug in Boto which includes this parameter even when
    it's not needed. Doing so causes S3 to respond with an AccessDenied
    response.

--- www/utils/s3.py
+++ www/utils/s3.py
@@ -4,6 +4,9 @@ with prefixed keys in S3.
 
 See http://tartarus.org/james/diary/2013/07/18/fun-with-django-storage-backends
 """
+import urlparse
+import urllib
+
 from django.core.files.storage import get_storage_class
 
 from storages.backends.s3boto import S3BotoStorage
@@ -22,6 +25,19 @@ class StaticRootS3BotoStorage(S3BotoStorage):
         name = super(StaticRootS3BotoStorage, self).save(name, content)
         self.local_storage._save(name, content)
         return name
+
+    def url(self, name):
+        # Work-around a bug in Boto which appends the amz-security-token query
+        # param to S3 static URLs (and causes them to stop working). See
+        # https://github.com/boto/boto/issues/1477
+        orig = super(StaticRootS3BotoStorage, self).url(name)
+        scheme, netloc, path, params, query, fragment = urlparse.urlparse(orig)
+        params = urlparse.parse_qs(query)
+        if 'x-amz-security-token' in params:
+            del params['x-amz-security-token']
+        query = urllib.urlencode(params)
+        return urlparse.urlunparse(
+            (scheme, netloc, path, params, query, fragment))
 
 
 class MediaRootS3BotoStorage(S3BotoStorage):
